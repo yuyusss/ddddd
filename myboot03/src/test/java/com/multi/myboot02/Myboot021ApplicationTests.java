be01c5f622e9b9646ddddd3559bcd8154e3a4348
yuyusss <97606045+yuyusss@users.noreply.github.com>
2022-05-15 21:36:39 +0900

    Update Myboot021ApplicationTests.java

--- myboot03/src/test/java/com/multi/myboot02/Myboot021ApplicationTests.java
+++ myboot03/src/test/java/com/multi/myboot02/Myboot021ApplicationTests.java
@@ -3,6 +3,7 @@ package com.multi.myboot02;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class Myboot021ApplicationTests {
 
